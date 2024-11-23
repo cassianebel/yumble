e5508f6a861b11789559273344e4415df082a8b7
Cassia <162936759+cassianebel@users.noreply.github.com>
2024-11-23 12:09:46 -0600

    fix url on page change

--- src/components/NutrientSearchResults.jsx
+++ src/components/NutrientSearchResults.jsx
@@ -1,16 +1,11 @@
 import { useEffect, useState, useMemo } from "react";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import RecipeCard from "./RecipeCard";
 import NutrientSearchForm from "./NutrientSearchForm";
 
 const NutrientSearchResults = () => {
   const [results, setResults] = useState([]);
   const [displayedResults, setDisplayedResults] = useState([]);
-  const location = useLocation();
-  const params = useMemo(
-    () => new URLSearchParams(location.search),
-    [location.search]
-  );
   const [searchParams, setSearchParams] = useSearchParams();
   const currentPage = parseInt(searchParams.get("page") || "1", 10);
   const calories = useMemo(
@@ -28,6 +23,7 @@ const NutrientSearchResults = () => {
   const fat = useMemo(() => searchParams.get("fat").split(","), [searchParams]);
 
   useEffect(() => {
+    console.log("Search params:", calories, carbs, protein, fat);
     const apiKey = import.meta.env.VITE_SPOONACULAR_API_KEY;
     const url = `https://api.spoonacular.com/recipes/findByNutrients?minCalories=${calories[0]}&maxCalories=${calories[1]}&minCarbs=${carbs[0]}&maxCarbs=${carbs[1]}&minProtein=${protein[0]}&maxProtein=${protein[1]}&minFat=${fat[0]}&maxFat=${fat[1]}&number=100&apiKey=${apiKey}`;
 
@@ -58,7 +54,14 @@ const NutrientSearchResults = () => {
   const totalPages = Math.ceil(results.length / itemsPerPage);
 
   const goToPage = (page) => {
-    setSearchParams({ calories, carbs, protein, fat, page });
+    console.log(calories, carbs, protein, fat);
+    setSearchParams({
+      calories: calories.join(","),
+      carbs: carbs.join(","),
+      protein: protein.join(","),
+      fat: fat.join(","),
+      page,
+    });
   };
 
   return (
